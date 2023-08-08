c5554f4f372f33623c9874210c2f546c936f2fd0
DenisAnimeshnik <pro100killer0001@gmail.com>
2023-08-08 18:12:17 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const themeSwitchBtn = document.querySelector('#theme-checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+    if (currentTheme === 'light') {themeSwitchBtn.checked = true}
+} else {
+    document.documentElement.setAttribute('data-theme', 'dark')
+}
+
+function switchTheme() {
+    let theme = themeSwitchBtn.checked ? 'light':'dark'
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+}
+
+themeSwitchBtn.addEventListener('change', switchTheme, false)
+switchTheme()

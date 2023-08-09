2f9297fa6ad9f7d8fd1d90b3ab269605200fa0a7
DenisAnimeshnik <pro100killer0001@gmail.com>
2023-08-09 16:01:52 +0600

    fix menu

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $('.content').load("main.html");
 
-window.onhashchange = function() { $('.content').load(window.location.hash.slice(1) + '.html'); }
+if (window.location.hash == '') {
+    window.location.hash = 'main'
+}
+
+function loadContent() {
+    $('.content').load(window.location.hash.slice(1) + '.html');
+}
+
+
+loadContent()
+window.onhashchange = loadContent

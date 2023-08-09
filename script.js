8f3944e1ef3c0fde40e25e31d89b857fe0661fb9
DenisAnimeshnik <pro100killer0001@gmail.com>
2023-08-09 15:47:03 +0600

    add normal page changer

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function changeLocation(attr) {
-    // let oldUrl = $(this).attr("href"); // Get current url
-    // let newUrl = oldUrl.replace("http://", "https://"); // Create new url
-    // $(this).attr("location", attr);
-    window.location.hash = attr
-}
-
-console.log($(location).attr('href'))
 $('.content').load("main.html");
 
 window.onhashchange = function() { $('.content').load(window.location.hash.slice(1) + '.html'); }

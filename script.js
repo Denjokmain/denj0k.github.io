258a0f2170945becfd3e9325a89c7801aa7214f9
DenisAnimeshnik <pro100killer0001@gmail.com>
2023-08-09 13:45:34 +0600

    add pages

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-$('.header').load("header.html");
+function changeLocation(attr) {
+    // let oldUrl = $(this).attr("href"); // Get current url
+    // let newUrl = oldUrl.replace("http://", "https://"); // Create new url
+    // $(this).attr("location", attr);
+    window.location.hash = attr
+}
+
+console.log($(location).attr('href'))
+$('.content').load("main.html");
+
+window.onhashchange = function() { $('.content').load(window.location.hash + '.html'); }
